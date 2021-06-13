39c41dd026ec0648c69eb69f027771b13fcf134b
Jakub Pelec <kuba1pelec@gmail.com>
2021-06-13 13:04:10 +0100

    INITIAL COMMIT

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_dance.contrib.github import make_github_blueprint, github
 
 app = Flask(__name__)
+
+app.secret_key = "supersekrit"
+blueprint = make_github_blueprint(
+    client_id="my-key-here",
+    client_secret="my-secret-here",
+)
+app.register_blueprint(blueprint, url_prefix="/login")
 
 from app import views
